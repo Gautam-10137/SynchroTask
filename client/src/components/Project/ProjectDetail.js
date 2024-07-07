3f7d5b73469843fadad1964f285521b385e01273
Gautam-10137 <pahwagautam786@gmail.com>
2024-07-07 23:18:21 +0530

    Chat Icon Added

--- client/src/components/Project/ProjectDetail.js
+++ client/src/components/Project/ProjectDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useProjects } from "../../context/ProjectContext";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -29,7 +29,8 @@ const ProjectDetail = () => {
     dueDate: "",
   });
   const { updateProject } = useProjects();
-
+  const [showChat,setShowChat]=useState(false);
+  const chatRef= useRef(null);
 
   useEffect(() => {
     console.log("projectID:");
@@ -119,86 +120,123 @@ const ProjectDetail = () => {
       console.error("Error adding task:", err);
     }
   };
+  
+  const handleChatIconClick=()=>{
+    
+    setShowChat(prevShowChat=> !prevShowChat);
+    
+    if (chatRef.current) {
+      console.log(!showChat);
+      chatRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+    else{
+      console.log(!showChat);
+    }
+    
+  }
 
   if (!project) {
     return <p className="text-gray-700">No project selected.</p>;
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg mt-6 border-2">
-      <h2 className="text-4xl font-bold mb-6 text-center text-purple-700">
-        {project.name}
-      </h2>
-      <p className="text-gray-700 mb-6 text-center">{project.description}</p>
-      <div className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4 text-blue-700">Members</h3>
-        <ul className="list-disc list-inside space-y-2">
-          {project.members.map((member, idx) => (
-            <li key={idx} className="text-gray-600 flex items-center">
-              <span className="font-medium">{member.userId.name}</span>
-              <span className="ml-2 text-gray-500">
-                ({member.userId.email})
-              </span>
-              <span className="ml-2 bg-yellow-200 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
-                {member.role}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4 text-green-700">Tasks</h3>
-        <ul className="space-y-4">
-          {project.tasks.map((task, idx) => (
-            <li
-              key={idx}
-              className="border border-gray-300 rounded-lg p-4 cursor-pointer"
-              onClick={() => handleTaskClick(task)}
-            >
-              <h4 className="font-semibold text-lg mb-2">{task.title}</h4>
-              <p className="text-gray-600">{task.description}</p>
-              <div className="flex items-center justify-between mt-2">
-                <span className="text-sm text-gray-500">
-                  Priority: {task.priority}
-                </span>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {showAddMemberDialog && (
-        <AddMemberDialog showMemberError={showMemberError}  handleAddMemberSubmit={handleAddMemberSubmit} newMemberEmail={newMemberEmail} setNewMemberEmail={setNewMemberEmail} newMemberRole={newMemberRole} setNewMemberRole={setNewMemberRole} setShowAddMemberDialog={setShowAddMemberDialog} />
-      )}
-
-      {selectedTask && (
-        <TaskDetailDialog task={selectedTask} onClose={handleCloseDialog} />
-      )}
-      
-
-      {(userRole === "admin" || userRole === "manager") && (
-        <div className="flex justify-center mt-4">
-          <button
-            onClick={handleAddMember}
-            className="btn bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 mr-4 rounded"
-          >
-            Add Member
-          </button>
-          <button
-            onClick={handleAddTask}
-            className="btn bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-          >
-            Add Task
-          </button>
-        </div>
-      )}
-
-      {showAddTaskDialog && (
-        <AddTaskDialog project={project} taskDetails={taskDetails} setTaskDetails={setTaskDetails} setShowAddTaskDialog={setShowAddTaskDialog} handleAddTaskSubmit={handleAddTaskSubmit}  />
-      )}
-
-     <Chat projectID={projectId} />
-
+    <div className="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg mt-6 border-2 relative">
+    <div className="flex justify-end">
+      <button
+        className="  h-full border-2 p-2 rounded-full hover:bg-gray-200"
+        onClick={handleChatIconClick}
+      >
+        <img src={require('../../static/chat.png')} alt="Chat" className="h-8 w-8" />
+      </button>
     </div>
+    <h2 className="text-4xl font-bold mb-6 text-center text-purple-700">
+      {project.name}
+    </h2>
+    <p className="text-gray-700 mb-6 text-center">{project.description}</p>
+    <div className="mb-8">
+      <h3 className="text-2xl font-semibold mb-4 text-blue-700">Members</h3>
+      <ul className="list-disc list-inside space-y-2">
+        {project.members.map((member, idx) => (
+          <li key={idx} className="text-gray-600 flex items-center">
+            <span className="font-medium">{member.userId.name}</span>
+            <span className="ml-2 text-gray-500">
+              ({member.userId.email})
+            </span>
+            <span className="ml-2 bg-yellow-200 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+              {member.role}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className="mb-8">
+      <h3 className="text-2xl font-semibold mb-4 text-green-700">Tasks</h3>
+      <ul className="space-y-4">
+        {project.tasks.map((task, idx) => (
+          <li
+            key={idx}
+            className="border border-gray-300 rounded-lg p-4 cursor-pointer"
+            onClick={() => handleTaskClick(task)}
+          >
+            <h4 className="font-semibold text-lg mb-2">{task.title}</h4>
+            <p className="text-gray-600">{task.description}</p>
+            <div className="flex items-center justify-between mt-2">
+              <span className="text-sm text-gray-500">
+                Priority: {task.priority}
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+    {showAddMemberDialog && (
+      <AddMemberDialog
+        showMemberError={showMemberError}
+        handleAddMemberSubmit={handleAddMemberSubmit}
+        newMemberEmail={newMemberEmail}
+        setNewMemberEmail={setNewMemberEmail}
+        newMemberRole={newMemberRole}
+        setNewMemberRole={setNewMemberRole}
+        setShowAddMemberDialog={setShowAddMemberDialog}
+      />
+    )}
+
+    {selectedTask && (
+      <TaskDetailDialog task={selectedTask} onClose={handleCloseDialog} />
+    )}
+
+    {(userRole === "admin" || userRole === "manager") && (
+      <div className="flex justify-center mt-4">
+        <button
+          onClick={handleAddMember}
+          className="btn bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 mr-4 rounded"
+        >
+          Add Member
+        </button>
+        <button
+          onClick={handleAddTask}
+          className="btn bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Add Task
+        </button>
+      </div>
+    )}
+
+    {showAddTaskDialog && (
+      <AddTaskDialog
+        project={project}
+        taskDetails={taskDetails}
+        setTaskDetails={setTaskDetails}
+        setShowAddTaskDialog={setShowAddTaskDialog}
+        handleAddTaskSubmit={handleAddTaskSubmit}
+      />
+    )}
+    
+    {/* {showChat &&  */}
+    <div ref={ chatRef } className="mt-4"> <Chat projectID={projectId}  /></div>
+     {/* } */}
+
+  </div>
   );
 };
 
