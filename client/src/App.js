0020795c4dfa423c245444eb8f20f2ba3c8bfd62
Gautam-10137 <pahwagautam786@gmail.com>
2024-07-10 16:03:20 +0530

    React Persistence storage done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Home from './components/Home/Home';
@@ -9,8 +9,23 @@ import { ProjectProvider } from './context/ProjectContext';
 import ProjectDetail from './components/Project/ProjectDetail';
 import CreateProject from './components/Project/CreateProject';
 import DashNavigator from './components/Dashboard/DashNavigator';
+import { useEffect } from 'react';
+import { loadUser } from './redux/authSlice';
+
 function App() {
  
+  const dispatch=useDispatch();
+  const isAuthenticated=useSelector((state)=>state.auth.isAuthenticated);
+  const loading =useSelector((state)=>state.auth.loading);
+
+  useEffect(()=>{
+    dispatch(loadUser());
+  },[dispatch]);
+
+  if(loading){
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="App m-1">
       <BrowserRouter>
@@ -26,7 +41,7 @@ function App() {
               {/* <Navigator/> */}
               <Routes>
                 
-                <Route path="/dashboard" element={<div><Dashboard/></div>} />
+                {isAuthenticated && <Route path="/dashboard" element={<div><Dashboard/></div>} />}
                 <Route path="/project/:projectId" element={<div><DashNavigator/><ProjectDetail /></div>}></Route>
                 <Route path="/createProject"  element={<div><Navigator page={"ProjectDetail"} /><CreateProject/></div>}></Route>
               </Routes>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,32 @@
-import {configureStore} from '@reduxjs/toolkit';
-import authReducer from './authSlice'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import authReducer from './authSlice';
+import { combineReducers } from 'redux';
 
-const store=configureStore({
-    reducer:{
-        auth:authReducer
-    }
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  // Add other reducers here
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+        ignoredPaths: ['register'], // Add any paths you want to ignore
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
